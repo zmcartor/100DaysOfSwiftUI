eee043098242d291dbfe9fe6073330dda2a43222
Zach McArtor <zmcartor@splunk.com>
2020-09-22 17:35:52 -0600

    Add WeSplit project src

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Zach McArtor on 9/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+    var body: some View {
+     
+      NavigationView {
+        Form {
+          
+          Section {
+            TextField("Amount", text: $checkAmount)
+              .keyboardType(.decimalPad)
+          
+            Picker("Number of people", selection: $numberOfPeople) {
+              ForEach(2..<100) {
+                Text("\($0) people")
+              }
+            }
+          }
+          
+          Section {
+            Text("$\(checkAmount)")
+          }
+          
+        }.navigationTitle("We Split")
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Zach McArtor on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
